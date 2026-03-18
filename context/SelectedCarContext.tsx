97f0975cf9253e2c88bfc42feb37f698f3bdb306
Nurzhan-dev <musabaevnurzhan@gmail.com>
2026-03-19 00:00:32 +0500

    Update SelectedCarContext.tsx

--- context/SelectedCarContext.tsx
+++ context/SelectedCarContext.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { createContext, useState, ReactNode } from "react";
+
 export const SelectedCarContext = createContext<any>(null);
 
 export const SelectedCarProvider = ({ children }: { children: ReactNode }) => {
   const [selectedCar, setSelectedCar] = useState<any>(null);
+  const [finalPrice, setFinalPrice] = useState<number | null>(null);
 
   return (
-    <SelectedCarContext.Provider value={{ selectedCar, setSelectedCar }}>
+    <SelectedCarContext.Provider value={{ selectedCar, setSelectedCar, finalPrice, setFinalPrice }}>
       {children}
     </SelectedCarContext.Provider>
   );
