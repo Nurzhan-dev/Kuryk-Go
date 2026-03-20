2ae07e1cc025c3884a51b8edb8cb03c85605b267
Nurzhan-dev <musabaevnurzhan@gmail.com>
2026-03-20 17:38:11 +0500

    update carslist

--- data/CarsList.tsx
+++ data/CarsList.tsx
@@ -2,29 +2,25 @@ export default[
     {
         id:1,
         name:'Легковой',
-        image:'/11.png',
-        charges:300,
+        image:'/taxi.webp',
         description:'Быстрые поездки по поселку и межгородские поездки до 70 км'
     },
     {
         id:2,
         name:'Газель',
-        image:'/газель.png',
-        charges:3000,
+        image:'/газель.webp',
         description: 'Перевозка мебели, стройматериалов и переезды'
     },
     {
         id: 3,
         name: 'Водовоз',
-        image: '/vodovoz.png',
-        charges: 5000,
+        image: '/vodovoz.webp',
         description: 'Доставка питьевой воды на дом'
     },
     {
         id: 4,
         name: 'Спецтехника',
-        image: '/спецтехника.png',
-        charges: 8000,
+        image: '/спецтехника.webp',
         description: 'Услуги эвакуатора, погрузчика и другой техники'
     }
 ]
