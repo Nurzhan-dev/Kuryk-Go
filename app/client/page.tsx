d66cf0260ab4e9a8f4a2262a1e05685885017a68
Nurzhan-dev <musabaevnurzhan@gmail.com>
2026-04-09 16:36:47 +0500

    Add useRouter import to page.tsx

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabaseClient";
 import ProtectedRoute from "@/components/ProtectedRoute";
 
